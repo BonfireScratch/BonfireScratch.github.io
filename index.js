6011b954f43635bc77212ca1359dc5163a5aebd3
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-05-13 19:36:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -31,3 +31,15 @@ function navbarTransition(mode) {
 		transition = true;
 	}
 }
+
+$(".dropdown-toggle").on("mouseenter", function () {
+    if (!$(this).parent().hasClass("show")) {
+        $(this).click();
+    }
+});
+
+$(".btn-group, .dropdown").on("mouseleave", function () {
+    if ($(this).hasClass("show")){
+        $(this).children('.dropdown-toggle').first().click();
+    }
+});
