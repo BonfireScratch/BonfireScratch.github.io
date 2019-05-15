c03d88ee557c3616dad378323e63c0a320e5cbc4
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-05-15 09:14:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var transition = false;
+var scroll;
+var onVerticalNavbar = 0;
 
 $(document).ready(function() {
 	navbarTransition('start');
@@ -20,33 +22,52 @@ $(window).scroll(function() {
 });
 
 function navbarTransition(mode) {
-	var scroll  = $(window).scrollTop();
+	scroll  = $(window).scrollTop();
 	
 	if (transition) {
 		$("#navbar").css('transition', 'all 1s');
 	}
 	
-	if (scroll <= 50) {
-		$("#navbar").removeClass("bg-white");
-		$("#navbar").removeClass("navbar-light");
-		$("#navbar").addClass("bg-transparent");
-		$("#navbar").addClass("navbar-dark");
-		$("#navbar").css("border-bottom", "");
-	} else {
-		$("#navbar").removeClass("bg-transparent");
-		$("#navbar").removeClass("navbar-dark");
-		$("#navbar").addClass("bg-white");
-		$("#navbar").addClass("navbar-light");
-		$("#navbar").css("border-bottom", "2px solid lightGray");
-	}
+	navColor(scroll <= 50);
 	
 	if (mode == 'scroll') {
 		transition = true;
 	}
 }
 
+function navColor(dark) {
+	if (onVerticalNavbar == 0) {
+		if (dark) {
+			navDark();
+		} else {
+			navWhite();
+		}
+	}
+}
+
+function navWhite() {
+	$("#navbar").removeClass("bg-transparent");
+	$("#navbar").removeClass("navbar-dark");
+	$("#navbar").addClass("bg-white");
+	$("#navbar").addClass("navbar-light");
+	$("#navbar").css("border-bottom", "2px solid lightGray");
+}
+
+function navDark() {
+	$("#navbar").removeClass("bg-white");
+	$("#navbar").removeClass("navbar-light");
+	$("#navbar").addClass("bg-transparent");
+	$("#navbar").addClass("navbar-dark");
+	$("#navbar").css("border-bottom", "");
+}
+
 $(".dropdown-toggle").on("mouseenter", function () {
     if (!$(this).parent().hasClass("show")) {
         $(this).click();
     }
+});
+
+$("#navbarToggler").on("mousedown", function () {
+	onVerticalNavbar = 1 - onVerticalNavbar;
+    navWhite();
 });
